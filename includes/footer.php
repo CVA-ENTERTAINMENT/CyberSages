4266c22d28fd176a5a7323fa61ae94aafa5081d4
Sivapravin <86611431+CVA-ENTERTAINMENT@users.noreply.github.com>
2024-10-16 00:47:19 +0800

    footer.php

--- includes/footer.php
+++ includes/footer.php
@@ -3,10 +3,7 @@
                     <div class="container-fluid px-4">
                         <div class="d-flex align-items-center justify-content-between small">
                             <div class="text-muted">Copyright &copy; <?php echo date('Y');?></div>
-                            <div>
-                                <a href="https://phpgurukul.com/" target="_blank">PHPGurukul | Programming Blog</a>
-    
-                            </div>
+
                         </div>
                     </div>
                 </footer>
